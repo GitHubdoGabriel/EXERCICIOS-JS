28979421a614a9958137c5225729d4631176701c
GitHubdoGabriel <gabriel.alves.gas@hotmail.com>
2021-04-19 21:04:13 -0300

    Create exercicio41.js

---
+++ exercicio41.js
@@ -0,0 +1,8 @@
+/*41) Crie uma função que retorna a string "Olá, " concatenada com um argumento text (a ser passado para a função) e com ponto de exclamação "!" no final.*/
+function cumprimentar(nome){
+    return `Olá, ${nome}!`
+}
+
+console.log(cumprimentar('Gabriel'))
+console.log(cumprimentar('Ana'))
+console.log(cumprimentar('Samara'))
