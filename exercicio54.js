f775fcc70cb13adc7a9d85e9066eb363d1e4fc3e
GitHubdoGabriel <gabriel.alves.gas@hotmail.com>
2021-05-13 21:51:39 -0300

    Create exercicio54.js

---
+++ exercicio54.js
@@ -0,0 +1,10 @@
+/*54)Desenvolva uma função que recebe como parâmetro um objeto e retorne um array de arrays, em que cada elemento é um array formado pelos pares chave/valor que corresponde a um atributo do objeto.*/
+
+function objetoParaArray(objeto) {
+    const resultado = []
+    for (let chave in objeto)
+    resultado.push([ chave, objeto[chave] ])
+    return resultado
+    }
+
+console.log(objetoParaArray({cachorro:'patas', galinha:'penas'}))    
