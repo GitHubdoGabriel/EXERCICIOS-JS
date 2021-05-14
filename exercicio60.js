96c965cb4a72142985451db4efc5419d39bf40d2
GitHubdoGabriel <gabriel.alves.gas@hotmail.com>
2021-05-14 15:54:55 -0300

    Create exercicio60.js

---
+++ exercicio60.js
@@ -0,0 +1,12 @@
+/*60)Faça uma função que recebe a base e a altura de um triângulo e retorne a área desse triângulo. A precisão
+deverá ser de duas casas decimais, arredondando se necessário.*/
+
+function areaDoTriangulo(base,altura){
+    resultado = (base * altura) / 2
+    total = resultado.toFixed(2)
+    return total
+}
+
+console.log(areaDoTriangulo(10,15))
+console.log(areaDoTriangulo(7,9))
+console.log(areaDoTriangulo(9.25,13.1))
